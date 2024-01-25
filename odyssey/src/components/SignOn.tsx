a812c30883bbf9accc629d2207e18adc1293b339
David Dye <ethandye34@gmail.com>
2024-01-24 20:24:51 -0500

    sign on update

--- odyssey/src/components/SignOn.tsx
+++ odyssey/src/components/SignOn.tsx
@@ -1,15 +1,24 @@
+import { useState } from "react";
+
 export default function SignOn() {
+  const [shouldLogIn, SetShouldLogIn] = useState<boolean>(false);
+
+  function logInClicked() {
+    console.log("we clicked baby");
+    SetShouldLogIn(true);
+  }
+
   return (
     <>
       <h1
-        class="mb-4 text-4xl font-extrabold leading-none tracking-tight  text-white md:text-5xl lg:text-6xl"
+        className="mb-4 text-4xl font-extrabold leading-none tracking-tight  text-white md:text-5xl lg:text-6xl"
         style={{ textAlign: "center", paddingTop: 10 }}
       >
         Welcome to Odyssey
       </h1>
 
       <div style={{ textAlign: "center" }}>
-        <p class="mb-6 text-lg font-normal sm:px-16 lg:text-xl xl:px-48 dark:text-gray-200">
+        <p className="mb-6 text-lg font-normal sm:px-16 lg:text-xl xl:px-48 dark:text-gray-200">
           Create Account
         </p>
         <form>
@@ -28,9 +37,16 @@ export default function SignOn() {
           <input type="email" id="email" name="email" />
         </form>
         <br />
-        <button class="rounded border border-blue-700 bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
-          Button
+        <button className="rounded border border-blue-700 bg-blue-500 px-2 py-1.5 font-bold text-white hover:bg-blue-700">
+          Create Account!
         </button>
+        <p>If you have an account, Click here to</p>
+        <a
+          className="cursor-pointer text-slate-100 hover:text-slate-500"
+          onClick={logInClicked}
+        >
+          Log in
+        </a>
       </div>
     </>
   );
