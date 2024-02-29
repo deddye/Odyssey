ee09984540ee3f82ae149ed35997532bb7686ff3
David Dye <ethandye34@gmail.com>
2024-02-28 21:22:35 -0500

    remove old create form

--- odyssey/src/components/landing/CreateAccount.tsx
+++ odyssey/src/components/landing/CreateAccount.tsx
@@ -1,7 +1,7 @@
 import { Label } from "~/components/ui/label";
 import { Input } from "~/components/ui/input";
 import { Button } from "~/components/ui/button";
-import { SetStateAction, useState } from "react";
+import { type SetStateAction, useState } from "react";
 import axios from "axios";
 
 const emailRegex = new RegExp(
