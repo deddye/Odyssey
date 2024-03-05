3374fd17ce0d054d02c6ce67c9b00ba2dfb0e105
David Dye <ethandye34@gmail.com>
2024-03-04 19:57:41 -0500

    add users to db, need to figure out blocking if email already in use

--- server/internal/routes/routes.go
+++ server/internal/routes/routes.go
@@ -10,11 +10,14 @@ import (
 	"github.com/deddye/odyssey/internal/structs"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
+	"github.com/google/uuid"
+	_ "github.com/lib/pq"
 )
 
 func GetRouter(db_url string) *chi.Mux {
 
-	db, err := sql.Open("postgres", db_url)
+	db, err := sql.Open(
+		"postgres", "user=postgres password=password dbname=users sslmode=disable")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -48,7 +51,41 @@ func GetRouter(db_url string) *chi.Mux {
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 		}
-		fmt.Printf("%s - %s - %s\n", user.Name, user.Password, user.Email)
+
+		//See if there is already a user with that email
+		// s := fmt.Sprintf("SELECT * FROM users WHERE email = '%q'", user.Email)
+		// rows, e1 := db.Query(s)
+
+		// if e1 != nil {
+		// 	log.Fatal(e1)
+		// }
+
+		// cols, e2 := rows.Columns()
+		// if e2 != nil {
+		// 	log.Fatal(e2)
+		// }
+
+		// if len(cols) != 0 {
+
+		// 	w.WriteHeader(http.StatusOK)
+		// 	w.Header().Set("Content-Type", "application/json")
+
+		// } else {
+
+		id := uuid.New()
+		fmt.Println(id)
+
+		sqlStatement := `
+			INSERT INTO users (name, email, password, id)
+			VALUES ($1, $2, $3, $4)`
+
+		_, db_err := db.Exec(sqlStatement, user.Name, user.Email, user.Password, id)
+
+		if db_err != nil {
+			log.Fatal(db_err)
+		}
+		fmt.Printf("Added user : %s - %s - %s\n", user.Name, user.Password, user.Email)
+		// }
 
 	})
 
